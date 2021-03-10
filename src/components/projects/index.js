278d10dd6b216cdd321b549cc2561de5b4eb962f
valAizcorbe <valeria.aizcorbe@hotmail.com>
2021-03-09 20:39:42 -0700

    project added - more info needed

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -5,17 +5,61 @@ import {FiCode} from 'react-icons/fi'
 import SingleProject from './singleProject';
 
 function Projects() {
+
+    const projects = [
+        {
+         
+            title: "Recipes",
+            video: 'blob:https://www.youtube.com/18ac89b1-3196-4c61-90e6-705dec996f3e',
+            description:'lala',
+    
+        },
+        {
+        
+            title: "Avalanche",
+            video: 'blob:https://www.youtube.com/18ac89b1-3196-4c61-90e6-705dec996f3e',
+            description:`Avalanche is an app that helps you calculate when you'll be debt free and start having savings. I created this website in 2 weeks when I had almost 2 months of experience using
+            REACT REDUX, REACT-ROUTER, JAVASCRIPT, CSS, HTML, SQL, NODE, EXPRESS, EXPRESS-SESSION, BCRYPT, MASSIVE
+            and
+            CHARTJS`,
+            
+        }, {
+          
+            title: "Artistry",
+            video: 'blob:https://www.youtube.com/18ac89b1-3196-4c61-90e6-705dec996f3e',
+            description:`Group Project:
+            Artistry is a social media website for Artists. They can sign in our website, and start posting about their art, videos and writing! They can 'save' the art that other account posted, make comments and just enjoy Artistry. We created this website using
+            REACT, REACT REDUX, REACT PLAYER, REACT-ROUTER, JAVASCRIPT, CSS, HTML, SQL, NODE, EXPRESS, EXPRESS-SESSION, BCRYPT
+            and
+            MASSIVE.`,
+            
+        }, {
+           
+            title: "Google-Chat",
+            video: 'blob:https://www.youtube.com/18ac89b1-3196-4c61-90e6-705dec996f3e',
+            description:`lala`,
+            
+        }, {
+            title: "Recipe - API",
+            video: 'blob:https://www.youtube.com/18ac89b1-3196-4c61-90e6-705dec996f3e',
+            description:`lala`,
+            
+        }
+    ]
+
     return(
         <Pbox theme={theme}>
             <Head><Title>My projects </Title>
             <a href='https://github.com/valAizcorbe'><Pbutton><FiCode/> All my work <AiOutlineGithub/></Pbutton></a> </Head>
 <Grid>
-<SingleProject/>
-<SingleProject/>
-<SingleProject/>
-<SingleProject/>
+    {projects.map(item => (
+        <SingleProject key={item.index}
+        title={item.title}
+        description={item.description}
+        video={item.video}/>
 
-<SingleProject/></Grid>
+    ))}
+</Grid>
 
         </Pbox>
     )
--- src/components/projects/singleProject/index.js
+++ src/components/projects/singleProject/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Case} from './style'
+import {Case} from './style';
 
-function SingleProject (){
+
+function SingleProject ({title, video, description}){
+
     return(
+          
         <Case>
-            This is a Project
-            <h1>Title</h1>
-            <div>This is a photo or video</div>
-            <p>Info about the project</p>
+            <h1>{title}</h1>
+            <div>{video}</div>
+            <p>{description}</p>
         </Case>
+        
+      
     )
 }
 
